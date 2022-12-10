8e276ff750f102b91e50b60cca0518f6dcaa9ce5
Carlos Collado <carlose0808@hotmail.com>
2022-12-09 20:24:50 -0400

    Fixed error

--- src/components/ComicDetails.jsx
+++ src/components/ComicDetails.jsx
@@ -1,7 +1,7 @@
 import { GetComicDetails } from "../services/comic-service";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 const ComicDetails = () => {
   const { id } = useParams();
@@ -16,16 +16,13 @@ const ComicDetails = () => {
 
   return (
     <Card style={{ width: "18rem" }} className="ml-4">
-      <Card.Img variant="top" src={comicDetails.image.small_url} />
+      <Card.Img variant="top" src={comicDetails?.image?.small_url} />
       <Card.Body>
-        <Card.Title>{comicDetails.volume.name}</Card.Title>
-        <Card.Text>Credits:</Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        {comicDetails.concept_credits.map((comic) => (
-          <ListGroup.Item>{comic.name}</ListGroup.Item>
+        <Card.Title>{comicDetails?.volume?.name}</Card.Title>
+        {comicDetails?.person_credits?.map((credit) => (
+          <Card.Text key={credit.id}>Credits to: {credit.name}</Card.Text>
         ))}
-      </ListGroup>
+      </Card.Body>
     </Card>
   );
 };
